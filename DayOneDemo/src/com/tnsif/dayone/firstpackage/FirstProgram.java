99fa1b69cad0cfe7ed206db93b6bae288551a987
HP <HP@LAPTOP-REUQ2DKO>
2025-01-13 17:37:03 +0530

    Day one

---
+++ DayOneDemo/src/com/tnsif/dayone/firstpackage/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.tnsif.dayone.firstpackage;
+
+public class FirstProgram {
+
+	
+	//main method 
+	public static void main(String[] args) {
+		
+		System.out.println("------Hello Everyone-----");
+		
+	
+	}
+
+}
